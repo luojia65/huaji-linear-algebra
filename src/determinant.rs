e99b6e46b9b09bd8702daddd09d73a571e80f2f8
luojia65 <me@luojia.cc>
2019-02-27 12:31:34 +0800

    Update

--- src/determinant.rs
+++ src/determinant.rs
@@ -1,16 +1,39 @@
+use core::{ops::{Add, Sub, Mul}};
+
 pub struct Det<T> {
-    order: usize,
-    arr: [T],
+    inner: [T]
 }
 
 impl<T> Det<T> {
-    pub fn new<A: AsRef<[T]> + ?Sized>(order: usize, arr: &A) -> &Det<T> {
-        // 卧槽，order怎么办？？
-        unsafe { &*(arr.as_ref() as *const [T] as *const Det<T>) }
+    pub fn new<A: AsRef<[T]>>(inner: &A) -> &Det<T> {
+        let inner = inner.as_ref();
+        assert!(inner.len().is_power_of_two(), "length of inner array must be power of two");
+        unsafe { &*(inner as *const [T] as *const Det<T>) }
     }
 
     pub fn as_slice(&self) -> &[T] {
-        &self.arr
+        &self.inner
+    }
+
+    pub fn as_mut_slice(&mut self) -> &mut [T] {
+        &mut self.inner
     }
 }
 
+impl<T> Det<T> {
+    pub fn cofactor(&self, i: usize, j: usize) -> &Det<T> {
+        unimplemented!()
+    }
+}
+
+impl<T> Det<T> 
+where T: Add + Sub + Mul {
+
+    pub fn arithmatic_cofactor(&self, i: usize, j: usize) -> &Det<T> {
+        unimplemented!()
+    }
+
+    pub fn calc(&self) -> T {
+        unimplemented!()
+    }
+}
